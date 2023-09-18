7a4bf07d624b7879ec6f1e6e360dfe4c488b925c
kamibrenda <mukamibrenda254@gmail.com>
2023-09-18 16:38:00 +0300

    Started Alien Invasion game project.

---
+++ alien_invasion.py
@@ -0,0 +1,45 @@
+import sys 
+
+import pygame
+
+from settings import settings
+from ship import ship
+
+def run_game():
+    #initialize pygame, settings and a screen object.
+    pygame.init()
+    ai_settings = settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    #make a ship
+    ship = ship(screen)
+
+    #set the background color
+    bg_color = (230, 230, 230)
+
+    #Start the main loop for the game.
+    while True:
+
+        #watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+                
+        #Redraw the screen during each pass through the loop
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+
+        #Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+run_game()
+
+# Check if the window is open by getting the surface
+if pygame.display.get_surface() is None:
+    print("The Pygame window is not open.")
+else:
+    print("The Pygame window is open.")
+
+    pygame.display.update()
+
---
+++ ship.py
@@ -0,0 +1,17 @@
+import pygame
+
+def __init__(self, screen):
+    """Initialize the ship and set its starting position."""
+
+    #Load the ship image and get its rect
+    self.image = pygame.image.load('images/ship.bmp')
+    self.rect = self.image.get_rect()
+    self.screen_rect = screen.get_rect()
+
+    #set each new ship at the bottom centre of the screen
+    self.rect.centerx = self.screen_rect.centerx
+    self.rect.bottom = self.screen_rect.bottom
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
