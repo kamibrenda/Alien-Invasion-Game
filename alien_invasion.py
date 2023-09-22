0a3a0131c35868c5502da8fa4fd86cbcd8e2fcf4
kamibrenda <mukamibrenda254@gmail.com>
2023-09-22 15:44:16 +0300

    Additional file changes in game_functions

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,7 +3,8 @@ import sys
 import pygame
 
 from settings import settings
-
+from ship import Ship
+import game_functions as gf
 
 def run_game():
     #initialize pygame, settings and a screen object.
@@ -12,11 +13,16 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    #Make a ship
+    ship = Ship(screen)
+
      #set the background color
     bg_color = (230, 230, 230)
 
     #Start the main loop for the game.
     while True:
+        gf.check_events(ship)
+        gf.update_screen(ai_settings, screen, ship)
 
         #watch for keyboard and mouse events.
         for event in pygame.event.get():
@@ -25,7 +31,8 @@ def run_game():
                 
         #Redraw the screen during each pass through the loop
         screen.fill(ai_settings.bg_color)
-       
+        ship.blitme()
+           
         #Make the most recently drawn screen visible.
         pygame.display.flip()
 
